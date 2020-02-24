db82e20527655d6e592e7148b6a9b8dc0b922825
whsperngangel <25974388+whsperngangel@users.noreply.github.com>
2020-02-24 13:30:48 +0800

    Create SupplierForm.cs

---
+++ SupplierForm.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace VehicleMonitoringSystem
+{
+    public partial class SupplierForm : Form
+    {
+        public SupplierForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
