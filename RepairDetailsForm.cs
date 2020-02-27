31b722d1adbd9d50866e20c2edcd2486a64d9c0e
whsperngangel <25974388+whsperngangel@users.noreply.github.com>
2020-02-27 15:58:56 +0800

    Update RepairDetailsForm.cs

--- RepairDetailsForm.cs
+++ RepairDetailsForm.cs
@@ -13,27 +13,67 @@ namespace VehicleMonitoringSystem
     public partial class RepairDetailsForm : Form
     {
         #region Variables
+        private string _invoiceNumber;
+        public int _repairID, _partID, _supplierID, _getID;
+        private DateTime _purchasedDate;
+        private double _amount, _totalAmt; //added
+        private bool _paid;
+        private Part _part = new Part();
+        private Supplier _supplier = new Supplier();
         private RepairDetail _repairdetail = new RepairDetail();
         List<RepairDetail> _repairdetails = new List<RepairDetail>();
+        private MainForm _mainForm;
         #endregion
         public RepairDetailsForm()
         {
             InitializeComponent();
+            LoadDetails();
+            
         }
-        internal RepairDetailsForm(int repairID)
+        //dito
+        internal RepairDetailsForm(MainForm mainForm)
         {
-
+            InitializeComponent();
+            _mainForm = mainForm;
+            LoadDetails();
         }
         public void LoadDetails()
         {
-            _repairdetails = _repairdetail.RetrieveRepairDetailList();
+            _getID = _mainForm._repairID;
+            _repairdetails = _repairdetail.RetrieveRepairDetailList(_getID);
             foreach (RepairDetail r in _repairdetails)
             {
-                detailListLV.Items.Add(r.RepairID.ToString());
+                _repairID = r.RepairID;
+                _partID = r.PartID;
+                _purchasedDate = r.PurchasedDate;
+                _amount = r.Amount;
+                _invoiceNumber = r.InvoiceNumber;
+                _supplierID = r.SupplierID;
+                _paid = r.Paid;
+
+                ListViewItem searchRepair = new ListViewItem(_repairID.ToString());
+                searchRepair.SubItems.Add(_part.RetrievePartInfo(_partID).Description);
+                searchRepair.SubItems.Add(_purchasedDate.ToShortDateString());
+                searchRepair.SubItems.Add(_supplier.RetrieveSupplierInfo(_supplierID).SupplierName);
+                searchRepair.SubItems.Add(_invoiceNumber);
+                searchRepair.SubItems.Add(_amount.ToString("N2"));
+
+                detailListLV.Items.Add(searchRepair);
             }
+            this.Compute();
         }
+        private void Compute()
+        {
+            foreach (RepairDetail r in _repairdetails)
+            {
+                _totalAmt += r.Amount;
+            }
+
+            partTotalTB.Text = "P " + _totalAmt.ToString("N2");
+        }
+        //doon
 
 
-     
+
     }
 }
