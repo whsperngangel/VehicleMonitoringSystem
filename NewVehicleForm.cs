3c7bfdcd64eb04971cb5335f00286326cadfc3e3
whsperngangel <25974388+whsperngangel@users.noreply.github.com>
2020-02-27 16:11:48 +0800

    Update NewVehicleForm.cs

--- NewVehicleForm.cs
+++ NewVehicleForm.cs
@@ -42,6 +42,12 @@ namespace VehicleMonitoringSystem
             InitializeComponent();
             ComboBoxLoad();
         }
+
+        internal NewVehicleForm(MainForm mainForm)
+        {
+            InitializeComponent();
+            _mainForm = mainForm;
+        }
         #endregion
 
         #region Vehicle Methods
@@ -58,34 +64,73 @@ namespace VehicleMonitoringSystem
 
         private void saveB_Click(object sender, EventArgs e)
         {
-            try
+            if (plateNumberTB.Text == "")
             {
-                plateNumber = plateNumberTB.Text.Trim();
-                registeredName = registeredNameTB.Text.Trim();
-                orNumber = orNumberTB.Text.Trim();
-                orDate = orDateDTP.Value;
-                crNumber = crNumberTB.Text.Trim();
-                crDate = crDateDTP.Value;
-                renewal = renewalDTP.Value;
-                listing = listingCB.Text.Trim();
-                brand = brandCB.Text.Trim();
-                category = categoryCB.Text.Trim();
-                amount = double.Parse(amountTB.Text.Trim());
-                issuedTo = issuedToTB.Text.Trim();
-                remarks = remarksRTB.Text.Trim();
-
-                _vehicle = new Vehicle(plateNumber, listing, brand, category, crNumber, crDate, issuedTo);
-                _registration = new Registration(registrationID,plateNumber, registeredName, orNumber, orDate, renewal, amount, remarks);
-
-                _vehicle.InsertVehicle(_vehicle);
-                _registration.InsertRegistration(_registration);
-                
-                this.Close();
-                _mainForm.ListViewLoad();
+                MessageBox.Show("Please enter a PLATE NUMBER", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
-            catch (Exception ex)
+            else if (registeredNameTB.Text == "")
             {
-                MessageBox.Show(ex.ToString());
+                MessageBox.Show("Please enter the REGISTERED NAME", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (orNumberTB.Text == "")
+            {
+                MessageBox.Show("Please enter the vehicle's OR NUMBER", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (crNumberTB.Text == "")
+            {
+                MessageBox.Show("Please enter the vehicle's CR NUMBER", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (listingCB.Text == "")
+            {
+                MessageBox.Show("Please choose a LISTING type", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (brandCB.Text == "")
+            {
+                MessageBox.Show("Please choose a BRAND", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (categoryCB.Text == "")
+            {
+                MessageBox.Show("Please choose a CATEGORY", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (amountTB.Text == "")
+            {
+                MessageBox.Show("Please insert the AMOUNT", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (issuedToTB.Text == "")
+            {
+                MessageBox.Show("Please write down to whome the vehicle is ISSUED TO", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                try
+                {
+                    plateNumber = plateNumberTB.Text.Trim();
+                    registeredName = registeredNameTB.Text.Trim();
+                    orNumber = orNumberTB.Text.Trim();
+                    orDate = orDateDTP.Value;
+                    crNumber = crNumberTB.Text.Trim();
+                    crDate = crDateDTP.Value;
+                    renewal = renewalDTP.Value;
+                    listing = listingCB.Text.Trim();
+                    brand = brandCB.Text.Trim();
+                    category = categoryCB.Text.Trim();
+                    amount = double.Parse(amountTB.Text.Trim());
+                    issuedTo = issuedToTB.Text.Trim();
+                    remarks = remarksRTB.Text.Trim();
+
+                    _vehicle = new Vehicle(plateNumber, listing, brand, category, crNumber, crDate, issuedTo);
+                    _registration = new Registration(registrationID, plateNumber, registeredName, orNumber, orDate, renewal, amount, remarks);
+
+                    _vehicle.InsertVehicle(_vehicle);
+                    _registration.InsertRegistration(_registration);
+
+                    this.Close();
+                    _mainForm.ListViewLoad();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
             }
         }
         private void cancelB_Click(object sender, EventArgs e)
