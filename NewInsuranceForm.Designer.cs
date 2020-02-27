f92d45d31a7eba06ab31c32bccad8270af9ca94c
whsperngangel <25974388+whsperngangel@users.noreply.github.com>
2020-02-27 15:49:28 +0800

    Create NewInsuranceForm.Designer.cs

---
+++ NewInsuranceForm.Designer.cs
@@ -0,0 +1,53 @@
+namespace VehicleMonitoringSystem
+{
+    partial class NewInsuranceForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox registeredNameTB;
+        private System.Windows.Forms.TextBox amountTB;
+        private System.Windows.Forms.DateTimePicker durationFromDTP;
+        private System.Windows.Forms.Button saveB;
+        private System.Windows.Forms.Button cencelB;
+        private System.Windows.Forms.Button clearB;
+        private System.Windows.Forms.DateTimePicker renewalDTP;
+        private System.Windows.Forms.DateTimePicker durationToDTP;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.ComboBox plateNumberCB;
+        private System.Windows.Forms.ComboBox companyCB;
+        private System.Windows.Forms.TextBox insuranceIDTB;
+    }
+}
