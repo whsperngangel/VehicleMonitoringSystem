04794eda94bb26940bda3482660e5a49f1339bc3
whsperngangel <25974388+whsperngangel@users.noreply.github.com>
2020-03-02 09:56:06 +0800

    Update NewPaymentForm.Designer.cs

--- NewPaymentForm.Designer.cs
+++ NewPaymentForm.Designer.cs
@@ -1,6 +1,6 @@
 namespace VehicleMonitoringSystem
 {
-    partial class NewPartForm
+    partial class NewPaymentForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,108 +28,252 @@ namespace VehicleMonitoringSystem
         /// </summary>
         private void InitializeComponent()
         {
-            this.addPartB = new System.Windows.Forms.Button();
-            this.partListLV = new System.Windows.Forms.ListView();
-            this.partIDHead = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.descriptionHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.label33 = new System.Windows.Forms.Label();
-            this.partDescriptionTB = new System.Windows.Forms.TextBox();
-            this.partIDTB = new System.Windows.Forms.TextBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NewPaymentForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.supplierCB = new System.Windows.Forms.ComboBox();
+            this.datePaidDTP = new System.Windows.Forms.DateTimePicker();
+            this.amountPaidTB = new System.Windows.Forms.TextBox();
+            this.saveB = new System.Windows.Forms.Button();
+            this.clearB = new System.Windows.Forms.Button();
+            this.cancelB = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.paidByCB = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.balanceTB = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.invoiceNumberCB = new System.Windows.Forms.ComboBox();
+            this.typeCB = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // addPartB
+            // label1
             // 
-            this.addPartB.Location = new System.Drawing.Point(320, 63);
-            this.addPartB.Margin = new System.Windows.Forms.Padding(4);
-            this.addPartB.Name = "addPartB";
-            this.addPartB.Size = new System.Drawing.Size(100, 28);
-            this.addPartB.TabIndex = 80;
-            this.addPartB.Text = "Add";
-            this.addPartB.UseVisualStyleBackColor = true;
-            this.addPartB.Click += new System.EventHandler(this.addPartB_Click);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Supplier:";
             // 
-            // partListLV
+            // label2
             // 
-            this.partListLV.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.partIDHead,
-            this.descriptionHeader});
-            this.partListLV.FullRowSelect = true;
-            this.partListLV.HideSelection = false;
-            this.partListLV.Location = new System.Drawing.Point(36, 99);
-            this.partListLV.Margin = new System.Windows.Forms.Padding(4);
-            this.partListLV.Name = "partListLV";
-            this.partListLV.Size = new System.Drawing.Size(384, 248);
-            this.partListLV.TabIndex = 78;
-            this.partListLV.UseCompatibleStateImageBehavior = false;
-            this.partListLV.View = System.Windows.Forms.View.Details;
-            this.partListLV.SelectedIndexChanged += new System.EventHandler(this.partListLV_SelectedIndexChanged);
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Date Paid:";
             // 
-            // partIDHead
+            // label3
             // 
-            this.partIDHead.Text = "ID";
-            this.partIDHead.Width = 46;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Amount Paid:";
             // 
-            // descriptionHeader
+            // supplierCB
             // 
-            this.descriptionHeader.Text = "Description";
-            this.descriptionHeader.Width = 237;
+            this.supplierCB.AllowDrop = true;
+            this.supplierCB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.supplierCB.Enabled = false;
+            this.supplierCB.FormattingEnabled = true;
+            this.supplierCB.Location = new System.Drawing.Point(160, 54);
+            this.supplierCB.Name = "supplierCB";
+            this.supplierCB.Size = new System.Drawing.Size(543, 28);
+            this.supplierCB.Sorted = true;
+            this.supplierCB.TabIndex = 3;
+            this.supplierCB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.supplierCB_KeyPress);
             // 
-            // label33
+            // datePaidDTP
             // 
-            this.label33.AutoSize = true;
-            this.label33.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label33.Location = new System.Drawing.Point(31, 28);
-            this.label33.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label33.Name = "label33";
-            this.label33.Size = new System.Drawing.Size(115, 25);
-            this.label33.TabIndex = 73;
-            this.label33.Text = "Description:";
+            this.datePaidDTP.Enabled = false;
+            this.datePaidDTP.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.datePaidDTP.Location = new System.Drawing.Point(151, 175);
+            this.datePaidDTP.Name = "datePaidDTP";
+            this.datePaidDTP.Size = new System.Drawing.Size(543, 26);
+            this.datePaidDTP.TabIndex = 4;
             // 
-            // partDescriptionTB
+            // amountPaidTB
             // 
-            this.partDescriptionTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.partDescriptionTB.Location = new System.Drawing.Point(154, 25);
-            this.partDescriptionTB.Margin = new System.Windows.Forms.Padding(4);
-            this.partDescriptionTB.Name = "partDescriptionTB";
-            this.partDescriptionTB.Size = new System.Drawing.Size(266, 30);
-            this.partDescriptionTB.TabIndex = 66;
+            this.amountPaidTB.Enabled = false;
+            this.amountPaidTB.Location = new System.Drawing.Point(151, 218);
+            this.amountPaidTB.Name = "amountPaidTB";
+            this.amountPaidTB.Size = new System.Drawing.Size(543, 26);
+            this.amountPaidTB.TabIndex = 5;
+            this.amountPaidTB.Text = "P 0.00";
+            this.amountPaidTB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.amountPaidTB_KeyPress);
             // 
-            // partIDTB
+            // saveB
             // 
-            this.partIDTB.Location = new System.Drawing.Point(154, 63);
-            this.partIDTB.Margin = new System.Windows.Forms.Padding(4);
-            this.partIDTB.Name = "partIDTB";
-            this.partIDTB.Size = new System.Drawing.Size(88, 22);
-            this.partIDTB.TabIndex = 81;
-            this.partIDTB.Visible = false;
+            this.saveB.Enabled = false;
+            this.saveB.Location = new System.Drawing.Point(457, 293);
+            this.saveB.Name = "saveB";
+            this.saveB.Size = new System.Drawing.Size(75, 27);
+            this.saveB.TabIndex = 6;
+            this.saveB.Text = "Save";
+            this.saveB.UseVisualStyleBackColor = true;
+            this.saveB.Click += new System.EventHandler(this.saveB_Click);
             // 
-            // NewPartForm
+            // clearB
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.clearB.Enabled = false;
+            this.clearB.Location = new System.Drawing.Point(619, 293);
+            this.clearB.Name = "clearB";
+            this.clearB.Size = new System.Drawing.Size(75, 27);
+            this.clearB.TabIndex = 7;
+            this.clearB.Text = "Clear";
+            this.clearB.UseVisualStyleBackColor = true;
+            this.clearB.Click += new System.EventHandler(this.clearB_Click);
+            // 
+            // cancelB
+            // 
+            this.cancelB.Enabled = false;
+            this.cancelB.Location = new System.Drawing.Point(538, 293);
+            this.cancelB.Name = "cancelB";
+            this.cancelB.Size = new System.Drawing.Size(75, 27);
+            this.cancelB.TabIndex = 8;
+            this.cancelB.Text = "Cancel";
+            this.cancelB.UseVisualStyleBackColor = true;
+            this.cancelB.Click += new System.EventHandler(this.cancelB_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(61, 262);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Paid By:";
+            // 
+            // paidByCB
+            // 
+            this.paidByCB.Enabled = false;
+            this.paidByCB.FormattingEnabled = true;
+            this.paidByCB.Location = new System.Drawing.Point(151, 259);
+            this.paidByCB.Name = "paidByCB";
+            this.paidByCB.Size = new System.Drawing.Size(543, 28);
+            this.paidByCB.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(68, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 20);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Balance:";
+            // 
+            // balanceTB
+            // 
+            this.balanceTB.Enabled = false;
+            this.balanceTB.Location = new System.Drawing.Point(151, 134);
+            this.balanceTB.Name = "balanceTB";
+            this.balanceTB.Size = new System.Drawing.Size(543, 26);
+            this.balanceTB.TabIndex = 5;
+            this.balanceTB.Text = "P 0.00";
+            this.balanceTB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.balanceTB_KeyPress);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 93);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Invoice Number:";
+            // 
+            // invoiceNumberCB
+            // 
+            this.invoiceNumberCB.AllowDrop = true;
+            this.invoiceNumberCB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.invoiceNumberCB.Enabled = false;
+            this.invoiceNumberCB.FormattingEnabled = true;
+            this.invoiceNumberCB.Location = new System.Drawing.Point(160, 90);
+            this.invoiceNumberCB.Name = "invoiceNumberCB";
+            this.invoiceNumberCB.Size = new System.Drawing.Size(543, 28);
+            this.invoiceNumberCB.Sorted = true;
+            this.invoiceNumberCB.TabIndex = 10;
+            // 
+            // typeCB
+            // 
+            this.typeCB.AllowDrop = true;
+            this.typeCB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.typeCB.FormattingEnabled = true;
+            this.typeCB.Items.AddRange(new object[] {
+            "Fuel",
+            "Part",
+            "Repair"});
+            this.typeCB.Location = new System.Drawing.Point(160, 12);
+            this.typeCB.Name = "typeCB";
+            this.typeCB.Size = new System.Drawing.Size(543, 28);
+            this.typeCB.Sorted = true;
+            this.typeCB.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(44, 15);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Type:";
+            // 
+            // NewPaymentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(454, 375);
-            this.Controls.Add(this.partIDTB);
-            this.Controls.Add(this.addPartB);
-            this.Controls.Add(this.partListLV);
-            this.Controls.Add(this.label33);
-            this.Controls.Add(this.partDescriptionTB);
-            this.Margin = new System.Windows.Forms.Padding(4);
-            this.MaximizeBox = false;
-            this.Name = "NewPartForm";
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(715, 335);
+            this.Controls.Add(this.typeCB);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.invoiceNumberCB);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cancelB);
+            this.Controls.Add(this.clearB);
+            this.Controls.Add(this.saveB);
+            this.Controls.Add(this.balanceTB);
+            this.Controls.Add(this.amountPaidTB);
+            this.Controls.Add(this.datePaidDTP);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.paidByCB);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.supplierCB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "NewPaymentForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "New Part Form";
+            this.Text = "Payment";
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button addPartB;
-        private System.Windows.Forms.ListView partListLV;
-        private System.Windows.Forms.ColumnHeader partIDHead;
-        private System.Windows.Forms.Label label33;
-        private System.Windows.Forms.TextBox partDescriptionTB;
-        private System.Windows.Forms.ColumnHeader descriptionHeader;
-        private System.Windows.Forms.TextBox partIDTB;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox supplierCB;
+        private System.Windows.Forms.DateTimePicker datePaidDTP;
+        private System.Windows.Forms.TextBox amountPaidTB;
+        private System.Windows.Forms.Button saveB;
+        private System.Windows.Forms.Button clearB;
+        private System.Windows.Forms.Button cancelB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox paidByCB;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox balanceTB;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox invoiceNumberCB;
+        private System.Windows.Forms.ComboBox typeCB;
+        private System.Windows.Forms.Label label7;
     }
 }
