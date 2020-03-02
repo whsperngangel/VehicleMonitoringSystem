7fa15167d2a089c321c0550303304a2a401dbcb7
whsperngangel <25974388+whsperngangel@users.noreply.github.com>
2020-03-02 10:25:30 +0800

    Update Statement.cs

--- Statement.cs
+++ Statement.cs
@@ -1,3 +1,4 @@
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,11 +13,10 @@ namespace VehicleMonitoringSystem
     {
         #region Variables
 
-        public int SupplierID;
-        public string InvoiceNumber;
-        public Double FuelAmountDue,
-            RepairAmountDue,
-            TotalDue;
+        public int SupplierID, ID, StatementID;
+        public string InvoiceNumber, Type, Status;
+        public Double AmountDue,
+                      TotalDue;
         public DateTime DateDue;
 
         DBOperation _dbOp = new DBOperation();
@@ -26,25 +26,44 @@ namespace VehicleMonitoringSystem
         #region Constructors
 
         public Statement() { }
-        public Statement(int supplierID, 
-                         string invoiceNumber, 
-                         Double fuelAmountDue, 
-                         Double repairAmountDue/*, DateTime dateDue*/)
+
+        public Statement(int statementID, int supplierID, String type, string invoiceNumber , string status)
         {
+            StatementID = statementID;
             SupplierID = supplierID;
+            Type = type;
             InvoiceNumber = invoiceNumber;
-            FuelAmountDue = fuelAmountDue;
-            RepairAmountDue = repairAmountDue;
-            //DateDue = dateDue;
+            Status = status;
+           
         }
-        //public Statement(Double totalDue)
-        //{
-        //    TotalDue = totalDue;
-        //}
         #endregion
 
         #region Statement Methods
+        public void InsertStatement(Statement statement)
+        {
+            try
+            {
+                _dbOp.DBConnect();
+                MySqlCommand cmdRepair = _dbOp._dbConn.CreateCommand();
 
+                cmdRepair.CommandText = @"INSERT INTO Statement(StatementID, SupplierID, Type, InvoiceNumber, Status) VALUES(@StatementID, @SupplierID, @Type, @InvoiceNumber,@Status)";
+
+                cmdRepair.Parameters.AddWithValue("@StatementID", statement.StatementID);
+                cmdRepair.Parameters.AddWithValue("@StatementID", statement.SupplierID);
+                cmdRepair.Parameters.AddWithValue("@Type", statement.Type);
+                cmdRepair.Parameters.AddWithValue("@InvoiceNumber", statement.InvoiceNumber);
+                cmdRepair.Parameters.AddWithValue("@Status", statement.Status);
+
+                cmdRepair.ExecuteNonQuery();
+
+                MessageBox.Show("New Repair Record has been saved!");
+                _dbOp.DBClose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
         public List<Statement> RetrieveStatementList(string conditionString1)
         {
             List<Statement> statements = new List<Statement>();
@@ -55,21 +74,18 @@ namespace VehicleMonitoringSystem
 
                 MySqlCommand cmd = _dbOp._dbConn.CreateCommand();
 
-                cmd.CommandText = @"SELECT supplier.SupplierID, repair.InvoiceNumber, fuel.Amount, repair.Amount FROM supplier INNER JOIN repair ON supplier.SupplierID=repair.SupplierID JOIN fuel ON fuel.SupplierID=repair.SupplierID " + conditionString1;
+                cmd.CommandText = @"SELECT * Statement " + conditionString1;
 
                 MySqlDataReader reader = cmd.ExecuteReader();
-
                 Statement temp = new Statement();
-
                 while (reader.Read())
                 {
-                    SupplierID = (int)reader.GetValue(0);
-                    InvoiceNumber = (string)reader.GetValue(1);
-                    FuelAmountDue = (Double)reader.GetValue(2);
-                    RepairAmountDue = (Double)reader.GetValue(3);
-                    //DateDue = (DateTime)reader.GetValue(3);
-
-                    temp = new Statement(SupplierID, InvoiceNumber, FuelAmountDue, RepairAmountDue/*, DateDue*/);
+                    StatementID = (int)reader.GetValue(0);
+                    SupplierID = (int)reader.GetValue(1);
+                    Type = (string)reader.GetValue(2);
+                    InvoiceNumber = (string)reader.GetValue(3);
+                    Status = (string)reader.GetValue(4);
+                    temp = new Statement(SupplierID, SupplierID, Type, InvoiceNumber, Status);
                     statements.Add(temp);
                 }
                 reader.Close();
@@ -79,13 +95,11 @@ namespace VehicleMonitoringSystem
             {
                 MessageBox.Show(ex.ToString());
             }
-
             return statements;
         }
-
-        public Statement RetrieveStatementInfo(int statementID)
+        /*public List<Statement> RetrieveRepairStatement(string conditionString1)
         {
-            Statement temp = new Statement();
+            List<Statement> statements = new List<Statement>();
 
             try
             {
@@ -93,68 +107,90 @@ namespace VehicleMonitoringSystem
 
                 MySqlCommand cmd = _dbOp._dbConn.CreateCommand();
 
-                cmd.CommandText = @"SELECT * FROM Statement " + "WHERE StatementID = @StatementID";
-                cmd.Parameters.AddWithValue("@StatementID", statementID);
+                cmd.CommandText = @"SELECT supplier.SupplierID, repair.RepairID, repair.InvoiceNumber, repair.Amount From Supplier INNER JOIN repair on supplier.SupplierID = repair.SupplierID " + conditionString1;
 
                 MySqlDataReader reader = cmd.ExecuteReader();
-
+                Statement temp = new Statement();
                 while (reader.Read())
                 {
                     SupplierID = (int)reader.GetValue(0);
-                    InvoiceNumber = (string)reader.GetValue(1);
-                    FuelAmountDue = (Double)reader.GetValue(2);
-                    RepairAmountDue = (Double)reader.GetValue(3);
-                    //DateDue = (DateTime)reader.GetValue(4);
-               
-                    temp = new Statement(SupplierID, InvoiceNumber, FuelAmountDue, RepairAmountDue/*, DateDue*/);
+                    ID = (int)reader.GetValue(1);
+                    InvoiceNumber = (string)reader.GetValue(2);
+                    AmountDue = (Double)reader.GetValue(3);
+                    Type = "Repair";
+                    temp = new Statement(StatementID, SupplierID, Type, InvoiceNumber, Status);
+                    statements.Add(temp);
                 }
                 reader.Close();
-
                 _dbOp.DBClose();
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString());
             }
-            return temp;
+            return statements;
+        }
+        public List<Statement> RetrieveRepairDetailStatement(string conditionString1)
+        {
+            List<Statement> statements = new List<Statement>();
+
+            try
+            {
+                _dbOp.DBConnect();
+
+                MySqlCommand cmd = _dbOp._dbConn.CreateCommand();
+
+                cmd.CommandText = @"SELECT supplier.SupplierID, repairdetail.PartID, repairdetail.InvoiceNumber, repairdetail.Amount FROM supplier INNER JOIN repairdetail ON supplier.SupplierID = repairdetail.SupplierID " //+ "WHERE Paid = false "// + conditionString1;
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+                Statement temp = new Statement();
+                while (reader.Read())
+                {
+                    SupplierID = (int)reader.GetValue(0);
+                    ID = (int)reader.GetValue(1);
+                    InvoiceNumber = (string)reader.GetValue(2);
+                    AmountDue = (Double)reader.GetValue(3);
+                    Type = "Part";
+                    temp = new Statement(SupplierID, ID, InvoiceNumber, Type, AmountDue);
+                    statements.Add(temp);
+                }
+                reader.Close();
+                _dbOp.DBClose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            return statements;
+        } */
+        public int CreateStatementID()
+        {
+            StatementID = CountStatement();
+
+            return StatementID;
         }
 
-        //public Statement GetTotal()
-        //{
-        //    Statement temp = new Statement(int statementID);
+        public int CountStatement()
+        {
+            int count = 1;
 
-        //    try
-        //    {
-        //        _dbOp.DBConnect();
+            _dbOp.DBConnect();
 
-        //        MySqlCommand cmd = _dbOp._dbConn.CreateCommand();
+            MySqlCommand cmd = _dbOp._dbConn.CreateCommand();
+            cmd.CommandText = @"SELECT * FROM Statement";
 
-        //        cmd.CommandText = @"SELECT(SELECT(SUM(Amount)) FROM fuel) + (SELECT(SUM(Amount)) FROM repair) FROM DUAL";
-        //        cmd.Parameters.AddWithValue("@StatementID", statementID);
+            MySqlDataReader reader = cmd.ExecuteReader();
 
-        //        MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                count++;
+            }
+            reader.Close();
+            _dbOp.DBClose();
 
-        //        while (reader.Read())
-        //        {
-        //            SupplierID = (int)reader.GetValue(0);
-        //            InvoiceNumber = (string)reader.GetValue(1);
-        //            FuelAmountDue = (Double)reader.GetValue(0);
-        //            RepairAmountDue = (Double)reader.GetValue(1);
-        //            DateDue = (DateTime)reader.GetValue(4);
+            return count;
 
-        //            temp = new Statement(SupplierID, InvoiceNumber, FuelAmountDue, RepairAmountDue/*, DateDue*/);
-        //        }
-        //        reader.Close();
-
-        //        _dbOp.DBClose();
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        MessageBox.Show(ex.ToString());
-        //    }
-        //    return temp;
-        //}
-
+        }
         #endregion
     }
 }
