cb35303dd2b7a7b2ea76cd007a61461646b67003
whsperngangel <25974388+whsperngangel@users.noreply.github.com>
2020-03-02 10:27:29 +0800

    Update NewSupplierForm.Designer.cs

--- NewSupplierForm.Designer.cs
+++ NewSupplierForm.Designer.cs
@@ -30,8 +30,6 @@ namespace VehicleMonitoringSystem
         {
             this.label1 = new System.Windows.Forms.Label();
             this.supplierTB = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.supplierIDTB = new System.Windows.Forms.TextBox();
             this.saveB = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -39,57 +37,41 @@ namespace VehicleMonitoringSystem
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(37, 44);
+            this.label1.Location = new System.Drawing.Point(28, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(90, 25);
+            this.label1.Size = new System.Drawing.Size(71, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Supplier:";
             // 
             // supplierTB
             // 
-            this.supplierTB.Location = new System.Drawing.Point(133, 48);
+            this.supplierTB.Location = new System.Drawing.Point(100, 39);
+            this.supplierTB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.supplierTB.Name = "supplierTB";
-            this.supplierTB.Size = new System.Drawing.Size(253, 22);
+            this.supplierTB.Size = new System.Drawing.Size(191, 20);
             this.supplierTB.TabIndex = 1;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 100);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(25, 17);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "ID:";
-            this.label2.Visible = false;
-            // 
-            // supplierIDTB
-            // 
-            this.supplierIDTB.Location = new System.Drawing.Point(43, 97);
-            this.supplierIDTB.Name = "supplierIDTB";
-            this.supplierIDTB.Size = new System.Drawing.Size(253, 22);
-            this.supplierIDTB.TabIndex = 1;
-            this.supplierIDTB.Visible = false;
             // 
             // saveB
             // 
-            this.saveB.Location = new System.Drawing.Point(311, 76);
+            this.saveB.Location = new System.Drawing.Point(233, 62);
+            this.saveB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.saveB.Name = "saveB";
-            this.saveB.Size = new System.Drawing.Size(75, 30);
+            this.saveB.Size = new System.Drawing.Size(56, 24);
             this.saveB.TabIndex = 3;
             this.saveB.Text = "Save";
             this.saveB.UseVisualStyleBackColor = true;
-            this.saveB.Click += new System.EventHandler(this.saveB_Click);
+            this.saveB.Click += new System.EventHandler(this.SaveB_Click);
             // 
             // NewSupplierForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(432, 126);
+            this.ClientSize = new System.Drawing.Size(324, 102);
             this.Controls.Add(this.saveB);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.supplierIDTB);
             this.Controls.Add(this.supplierTB);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "NewSupplierForm";
             this.Text = "Add New Supplier";
             this.ResumeLayout(false);
@@ -101,8 +83,6 @@ namespace VehicleMonitoringSystem
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox supplierTB;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox supplierIDTB;
         private System.Windows.Forms.Button saveB;
     }
 }
